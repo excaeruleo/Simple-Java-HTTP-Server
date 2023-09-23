f1a58675bfe784d5543ea7fceffd2751006341d9
David Huang <108887691+excaeruleo@users.noreply.github.com>
2023-09-23 13:17:13 -0400

    Update SimpleHTTPServer.java

--- SimpleHTTPServer.java
+++ SimpleHTTPServer.java
@@ -3,7 +3,7 @@ import java.net.Socket;
 import java.util.Date;
 public class SimpleHTTPServer {
     public static void main(String[] args) throws Exception {
-        final ServerSocket server = new ServerSocket(8080);
+        ServerSocket server = new ServerSocket(8080);
         System.out.println("Listening for connection on port 8080...");
         while (true) {
             try (Socket socket = server.accept()) {
